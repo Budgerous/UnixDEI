41803a850059f0ccc1148116d5051abf5d3e1657
Pedro Janeiro <pjaneiro@student.dei.uc.pt>
2015-10-27 23:06:05 +0000

    Added Linkedin links

--- index.js
+++ index.js
@@ -20,61 +20,61 @@ $(function() {
 
 $('#event0').popover({
     content: "O que é o Unix, como surgiu, e o que foi desenvolvido a partir dele.",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
 $('#event1').popover({
     content: "Aprende como podes contribuir para o mundo Linux a partir do testemunho de um colega teu.",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
 $('#event2').popover({
     content: "Lanche para ajudar a digerir tanta informação!",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
 $('#event3').popover({
     content: "Conheces uma ferramenta útil e gostavas de partilhá-la com os teus colegas? Esta é a tua oportunidade! Fala connosco e podes apresentar a tua ferramenta neste espaço.",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
 $('#event4').popover({
     content: "Conheces uma ferramenta útil e gostavas de partilhá-la com os teus colegas? Esta é a tua oportunidade! Fala connosco e podes apresentar a tua ferramenta neste espaço.",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
 $('#event5').popover({
     content: "Ementa a definir",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
 $('#event6').popover({
     content: "Para ti isto é tudo a mesma coisa? Nem sabes o que é? Vem perceber a diferença e como estes conceitos mudaram o mundo das aplicações livres.",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
 $('#event7').popover({
     content: "Conheces uma ferramenta útil e gostavas de partilhá-la com os teus colegas? Esta é a tua oportunidade! Fala connosco e podes apresentar a tua ferramenta neste espaço.",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
 $('#event8').popover({
     content: "Painel onde poderás ouvir os teus colegas a discutir sobre qual a melhor distribuição de Linux, e as suas principais diferenças.",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
 $('#event9').popover({
     content: "Tudo o que é bom acaba. Junta-te a nós para acabar este evento em grande.",
-    placement: "bottom",
+    placement: "right",
     trigger: "hover",
     viewport: "#events"
 });
